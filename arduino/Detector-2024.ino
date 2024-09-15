ac7fe67db2d8498e6c45ad8a3f15168a8447c502
Andrew Kim <107439239+i30101@users.noreply.github.com>
2024-09-15 16:17:28 -0400

    changes

--- arduino/Detector-2024.ino
+++ arduino/Detector-2024.ino
@@ -1,39 +1,17 @@
-#include <LiquidCrystal_I2C.h>
-#include <Wire.h>
-
-
-#define RED_PIN 5
-#define GREEN_PIN 6
+#define RED_PIN 10
+#define GREEN_PIN 8
 #define BLUE_PIN 7
-#define POT_PIN A0
+#define IN_PIN A3
 
+const int delayTime = 2;
+const int numReadings = 1000;
+int readings[numReadings];
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+int getRawADC() { return analogRead(IN_PIN); }
 
-
-/**
- * @return voltage value ranging from 0 to 5000
- */
-int getPotReading() {
-  float input = analogRead(POT_PIN);
-  return int(input * 5000.0 / 1024.0);
-}
-
-int getRawADC() {
-  return analogRead(A3);
-}
-
-void red() {
-  digitalWrite(RED_PIN, HIGH);
-}
-
-void green() {
-  digitalWrite(GREEN_PIN, HIGH);
-}
-
-void blue() {
-  digitalWrite(BLUE_PIN, HIGH);
-}
+void red() {digitalWrite(RED_PIN, HIGH); }
+void green() {digitalWrite(GREEN_PIN, HIGH); }
+void blue() {digitalWrite(BLUE_PIN, HIGH); }
 
 void clear() {
   digitalWrite(RED_PIN, LOW);
@@ -45,55 +23,47 @@ void setup() {
   pinMode(RED_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(BLUE_PIN, OUTPUT);
-  pinMode(POT_PIN, INPUT);
+
+  // configure input
+  pinMode(A3, INPUT);
+  analogReadResolution(14);
+
   Serial.begin(9600);
-
-  // lcd.init();
-  // lcd.backlight();
-
-  // set up ADC for pin 14
-  // analogReadResolution(14);
 }
 
 void loop() {
-  int reading = getRawADC();
-  Serial.println(reading);
-  red();
-  green();
-  blue();
-  clear();
+  
+  int sum = 0;
+  for (int i = 0; i < numReadings; i++) {
+    readings[i] = analogRead(A3);
+    sum += readings[i];
+    delay(delayTime);
+  }
 
-  // if (reading < 2048) {
-  //   clear();
-  // } else if (reading < 4096) {
-  //   clear();
-  //   red();
-  // } else if (reading < 6144) {
-  //   clear();
-  //   green();
-  // } else if (reading < 8192) {
-  //   clear();
-  //   blue();
-  // } else if (reading < 10240) {
-  //   clear();
-  //   green();
-  //   red();
-  // } else if (reading < 12288) {
-  //   clear();
-  //   blue();
-  //   green();
-  // } else if (reading < 14336) {
-  //   clear();
-  //   red();
-  //   green();
-  // } else {
-  //   clear();
-  //   red();
-  //   green();
-  //   blue();
-  // // }
-  lcd.clear();
-  lcd.setCursor(9, 0);
-  lcd.print(9);
-  // delay(100);
+  // calculate average raw ADC value
+  double average = sum / numReadings;
+
+  Serial.println("\nTAKING READING");
+
+  // print average raw ADC value
+  Serial.println("Average raw: " + String(average));
+
+  // print voltage
+  Serial.println("Voltage: " + String(average * 5.0 / 16383));
+
+  float ppm = 13.42 * average - 6884.3;
+
+  // print concentration in PPM
+  Serial.println("PPM: " + String (ppm));
+
+  // change LED status
+  if ((ppm > 625 && ppm < 2500) || (ppm > 4375)) {
+    clear();
+    red();
+  } else if ((ppm < 1875) || (ppm > 2500 && ppm < 3125)) {
+    clear();
+    green();
+  } else {
+    blue();
+  }
 }
